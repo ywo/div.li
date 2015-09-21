f755fd3b4b67a4b2d2c2e00b3f1e0012006207b1
ratly <lyj520@gmail.com>
2015-09-21 20:10:03 +0800

    init_thinkjs

---
+++ App/Lib/Controller/Api/VelidationController.js
@@ -0,0 +1,15 @@
+/**
+ * controller
+ * @return
+ */
+module.exports = Controller("Home/BaseController", function(){
+  "use strict";
+  return {
+    indexAction: function(){
+      //render View/Home/index_index.html file
+      // this.display();
+      let a = 10;
+      this.end(a)
+    }
+  };
+});
